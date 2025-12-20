1a83ffa47a9de31d5ae0a5fbce61e2cb2cb7b141
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-12-20 14:40:05 +0700

    Create script.js

---
+++ exit/script.js
@@ -0,0 +1,35 @@
+// ====================== NÚT THOÁT =========================
+window.openExitModal = function() {
+  if(!isSaveHidden) {
+    document.getElementById("warning-text-before").textContent = "Nếu bạn thoát,";
+    document.getElementById("warning-text-after").textContent = "thông tin chỉnh sửa sẽ không sao lưu";
+  } else {
+    document.getElementById("warning-text-before").textContent = "Bấm thoát";
+    document.getElementById("warning-text-after").textContent = "để trở lại màn hình chính!";
+    isSaveHidden=true;
+  }
+  const modal = document.getElementById('exit-modal');
+  modal.classList.remove('hidden');
+}
+
+window.closeExitModal = function() {
+  const modal = document.getElementById('exit-modal');
+  modal.classList.add('hidden');
+}
+window.confirmExit = function() {
+  unloadQuizCSS();
+  loadQuizCSS('style.css');
+  cleanupQuizDOM();
+  // dừng timer nếu có
+  stopTimer?.();
+  document.getElementById('app').style.display='block';
+  // Xóa HTML cũ
+  document.getElementById("quiz-root").innerHTML = "";
+}
+
+window.history.pushState(null, '', location.href);
+
+window.onpopstate = function() {
+  openExitModal();
+  window.history.pushState(null, '', location.href);
+};
